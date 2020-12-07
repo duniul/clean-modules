a3ac03b17f6fdca1fee8d0a1dd271685629d8a2c
Daniel Grefberg <hello@danielgrefberg.com>
2020-12-07 23:50:50 +0100

    chore: add eslint and prettier

---
+++ release.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  branches: ['master'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'yarn.lock', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/npm',
+  ],
+};
